35936df394bb39f5d284a9e016d6b02054eb8e2c
Tenji767 <144552671+Tenji767@users.noreply.github.com>
2026-03-03 15:49:46 -0500

    idk

--- admin/admin-login.php
+++ admin/admin-login.php
@@ -28,7 +28,42 @@ include("admin-server.php");
         <button type="submit" class="btn" name="login_user">Login</button>
     </div>
 
+<?php
 
+session_start();
+$username = "";
+$email = "";
+$errors = array();
+
+
+//Login User
+if(isset($_POST['login_user'])) {
+    $username=mysqli_real_escape_string($db, $_POST['username']);
+    $password=mysqli_real_escape_string($db, $_POST['password']);
+
+    if(empty($username)) {
+        array_push($errors, "Username is Required");
+    }
+    if(empty($password)) {
+        array_push($errors, "Password is Required");
+    }
+
+    if(count($errors) == 0) {
+        $password=md5($password);
+        $query="SELECT * FROM users WHERE username='$username' AND password='$password'";
+        $results=mysqli_query($db, $query);
+        if(mysqli_num_rows($results) == 1) {
+            $_SESSION['username'] = $username;
+            $_SESSION['success'] = "You are now logged in";
+            header('location: index.php');
+        }else {
+            array_push($errors, "Wrong Username/Password Combination");
+        }
+    }
+}
+
+
+?>
 
 
 </html>
