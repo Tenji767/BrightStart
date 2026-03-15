8f78bb9430958422aae49c4209a51688d35dda88
CalebMcHaney <chmchaney@liberty.edu>
2026-03-15 15:44:17 -0400

    added error reporting and database connection

--- questions.php
+++ questions.php
@@ -1,3 +1,22 @@
 <!-- Author: Caleb McHaney -->
- <!-- Questions.php is the main file for handling questions -->
-<!-- Tasks for me: integrate with the database -->
+<!-- Questions.php is the main file for handling questions -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<!-- reports errors and displays them -->
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1); // development error checking
+?>
+
+ <?php
+//Database connection
+$conn = new mysqli("sql112.infinityfree.com", "if0_41201125", "EvKOulpa615P", "if0_41201125_brightstar_db");
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+?>
