183aff669b1e0d909a65757f23364e0f5c6a7257
Noah Reynolds <npreynolds@liberty.edu>
2026-03-31 11:22:26 -0400

    Refactor admin dashboard: enhance layout, add school selection, and implement real-time clock

---
+++ unused/admin-dashboard.php
@@ -0,0 +1,32 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Admin Dashboard</title>
+    
+</head>
+
+
+<body>
+
+ <div class="admin-menu">
+
+    <a href="admin-lesson-create.php"><button class="admin-menu-item">Upload/Create Lessons</button></a>
+    <a href="tutor-manage.php"><button class="admin-menu-item">Manage Tutor Credentials</button></a>
+    <a href="student-account-manage.php"><button class="admin-menu-item">Manage School and Student Accounts</button></a>
+
+ </div>
+
+</body>
+
+
+
+</html>
+
+<!-- By Benjamin Nguyen -->
