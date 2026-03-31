7f2f9607d17d3449273277655a60b05a4aa81c88
Tenji767 <144552671+Tenji767@users.noreply.github.com>
2026-03-31 11:30:08 -0400

    changed nav a bit

--- index.php
+++ index.php
@@ -43,20 +43,8 @@ if(!isset($_SESSION['user_id'])) {
     <link rel="stylesheet" href="style.css">
 </head>
 
-<header>
-    <a href="index.php"><img src="logo.png" alt="Brightstart logo"/></a>
-    <h1>BrightStart Learning</h1>
-    <nav>
-        <ul>
-            <li><a href="lessons.php">Learn</a></li>
-            <li><a href="practice.php">Practice</a></li>
-            <li><a href="helper.html">Helper</a></li>
- 
-        </ul>
-        
-    </nav>
-    <a href="account.html"><img src="pfp.png"/></a>
-</header>
+<?php include('includes/nav.php');?>
+
 
 <body>
 
