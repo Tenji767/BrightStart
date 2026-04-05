8709aa938a01715c101409fadb2584d1da4698ff
Nick-DeBlock <nbdeblock@liberty.edu>
2026-04-05 19:53:47 -0400

    Add files via upload

--- includes/nav.php
+++ includes/nav.php
@@ -1,5 +1,6 @@
 <!--nav.php include created by Jordan Munster --> 
 <!-- Updated nav.php to include profile picture from the account.php by Noah Reynolds (Lines 3-8 and edited 21) -->
+ <!-- removed porfile picture, unable to display at this time can be reinstated later (Nick DeBlock) -->
 <?php
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
@@ -13,11 +14,12 @@ $navProfilePicture = $_SESSION['profile_picture'] ?? 'pfp.png';
         <ul>
             <li><a href="index.php">Home</a></li>
   
-            <li><a href="helper.php">Helper</a></li>
- 
+            <li><a href="helper2.php">Helper</a></li>
+
+            <li><a href="account.php">Account</a></li>
         </ul>
         
     </nav>
-    <a href="account.php">Account</a>
+    
 </header>
 <!-- nav updated to include only two links (lines 7-9) by Benjamin Nguyen -->
