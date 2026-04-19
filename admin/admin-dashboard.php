d830b6f03c8a9bae14a82c438cd7a8204a4e5ed6
Jordan Munster <twinb03@gmail.com>
2026-04-19 17:51:06 -0400

    testing putting style directly into php..might change if it doesn't work

--- admin/admin-dashboard.php
+++ admin/admin-dashboard.php
@@ -29,6 +29,72 @@ if(!isset($_SESSION['user_id']) || ($_SESSION['role'] ?? '') !== "admin"){
         <a href="manage-tutors.php"><button>Manage Tutors</button></a>
         <a href="manage-students.php"><button>Manage Students</button></a>
     </div>
+<style>
+body {
+  margin: 0;
+  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  background: #f4f7fb;
+  align-items: center !important;
+}
+
+/* Header */
+.header {
+  display: flex;
+  align-items: center;
+  padding: 25px 40px;
+}
+
+h1 {
+  font-size: 36px;
+  color: #1e293b;
+  text-align: left;
+}
+
+/* Home button */
+.home-btn {
+  background: #2563eb;
+  color: white;
+  border: none;
+  top: 10px;
+  padding: 12px 18px;
+  border-radius: 10px;
+  font-size: 16px;
+  font-weight: 600;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+.home-btn:hover {
+  background: #1d4ed8;
+}
+
+
+/* Buttons */
+.button-group {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  margin-top: 30px;
+}
+
+.button-group button {
+  width: 180px;
+  height: 120px;
+  border: none;
+  border-radius: 18px;
+  font-size: 18px;
+  font-weight: bold;
+  color: white;
+  background: #2563eb;
+  cursor: pointer;
+  box-shadow: 0 8px 18px rgba(0,0,0,0.1);
+  transition: 0.3s;
+}
+
+.button-group button:hover {
+  transform: translateY(-3px);
+}
+</style>
 </body>
 
 
