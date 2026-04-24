d31b67b96e07a3459023d3925b8ce370d9ff6a43
Jordan Munster <twinb03@gmail.com>
2026-04-23 21:33:47 -0400

    testing edit

--- tutor/tutor-account.php
+++ tutor/tutor-account.php
@@ -3,7 +3,7 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['user_id']) || ($_SESSION['role'] ?? '') !== 'tutor') {
+if (!isset($_SESSION['user_id']) || ($_SESSION['role'] ?? '') !== 'teacher') {
     header("Location: login.php");
     exit();
 }
